9d05fe4f815b7633d185dfc3791359cd7c8cbc9b
Sebastian Vargas <vargaspizango1@gmail.com>
2021-07-04 01:46:18 -0500

    feat: lógica de medidores añadida

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { PagesRoutingModule } from './pages-routing.module';
-import { RouterModule } from '@angular/router';
+import { ComponentsModule } from '../components/components.module';
+
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
 import { ProgressComponent } from './progress/progress.component';
@@ -10,6 +13,11 @@ import { ProgressComponent } from './progress/progress.component';
 @NgModule({
   declarations: [DashboardComponent, Grafica1Component, ProgressComponent],
   exports: [DashboardComponent, Grafica1Component, ProgressComponent],
-  imports: [CommonModule, RouterModule, PagesRoutingModule],
+  imports: [
+    CommonModule,
+    ComponentsModule,
+    RouterModule,
+    PagesRoutingModule,
+  ],
 })
 export class PagesModule {}
