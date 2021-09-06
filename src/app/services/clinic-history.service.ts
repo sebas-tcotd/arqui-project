8c6b598e6d95186c706734876aa4fa8163609c82
Sebastian Vargas <vargaspizango1@gmail.com>
2021-09-06 17:29:52 -0500

    minor: changed url backend

--- src/app/services/clinic-history.service.ts
+++ src/app/services/clinic-history.service.ts
@@ -3,6 +3,8 @@ import { HttpClient } from '@angular/common/http';
 import { catchError, map, retry } from 'rxjs/operators';
 import { ClinicHistoryResponse } from '../models/clinicHistory.model';
 
+const url = 'http://143.244.212.218:5000';
+
 @Injectable({
   providedIn: 'root',
 })
@@ -10,17 +12,12 @@ export class ClinicHistoryService {
   constructor(private http: HttpClient) {}
 
   createClinicHistory(body: any) {
-    return this.http.post(
-      'https://cm-sgd-histories.herokuapp.com/history',
-      body
-    );
+    return this.http.post(`${url}/history`, body);
   }
 
   loadClinicHistories() {
     return this.http
-      .get<ClinicHistoryResponse>(
-        'https://cm-sgd-histories.herokuapp.com/histories'
-      )
+      .get<ClinicHistoryResponse>(`${url}/histories`)
       .pipe(map((res) => res.histories));
   }
 }
