cb3b2b7e59d48d6c5a6867356cb6d68056a9da8a
Sebastian Vargas <vargaspizango1@gmail.com>
2021-09-06 22:08:43 -0500

    fix: add url sanitizer

--- src/app/services/clinic-history.service.ts
+++ src/app/services/clinic-history.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { catchError, map, retry } from 'rxjs/operators';
 import { ClinicHistoryResponse } from '../models/clinicHistory.model';
+import { DomSanitizer } from '@angular/platform-browser';
 
 const url = 'http://143.244.212.218:5000';
 
@@ -9,15 +10,17 @@ const url = 'http://143.244.212.218:5000';
   providedIn: 'root',
 })
 export class ClinicHistoryService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private sanitizer: DomSanitizer) {}
+
+  trustedUrl = this.sanitizer.bypassSecurityTrustUrl(url);
 
   createClinicHistory(body: any) {
-    return this.http.post(`${url}/history/`, body);
+    return this.http.post(`${this.trustedUrl}/history/`, body);
   }
 
   loadClinicHistories() {
     return this.http
-      .get<ClinicHistoryResponse>(`${url}/histories/`)
+      .get<ClinicHistoryResponse>(`${this.trustedUrl}/histories/`)
       .pipe(map((res) => res.histories));
   }
 }
